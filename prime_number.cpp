56a74e7a68c976fda1f95cbf082e4fe4a88164da
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2022-10-12 12:00:59 -0700

    #include<bits/stdc++.h> using namespace std; int main(){     int n,k=0;     cin>>n;     for(int i=2;i*i<n;i++)     {         if(n%i==0)         k=1;     }     if(k==0)     {         cout<<"prime";     }     else{         cout<<"not a prime";     }
    }

--- prime_number.cpp
+++ prime_number.cpp
@@ -3,20 +3,19 @@ using namespace std;
 int main(){
     int n,k=0;
     cin>>n;
-    for(int i=2;i<n;i++)
+    for(int i=2;i*i<n;i++)
     {
         if(n%i==0)
-        {
-            k=1;
-        }
-    }if(k==0)
+        k=1;
+    }
+    if(k==0)
     {
         cout<<"prime";
     }
-    else
-    {
+    else{
         cout<<"not a prime";
     }
+    
     
 }
 
