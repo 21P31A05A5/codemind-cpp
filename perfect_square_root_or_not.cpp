4c0239aec3e177c419f5206ec32cd31ef6f9fb48
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2022-10-12 12:29:07 -0700

    #include<bits/stdc++.h> using namespace std; int main(){     int n;     int s;     cin>>n;     s=sqrt(n);     if(s*s==n)     {         cout<<"True";     }     else     {         cout<<"False";     }
    }

---
+++ perfect_square_root_or_not.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    int s;
+    cin>>n;
+    s=sqrt(n);
+    if(s*s==n)
+    {
+        cout<<"True";
+    }
+    else
+    {
+        cout<<"False";
+    }
+     
+}
+
