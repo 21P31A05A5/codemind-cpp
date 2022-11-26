1fcb50e75c9da7703059aa1136d8e4feb0db3bb1
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2022-11-26 01:56:36 -0800

    #include<iostream> using namespace std; main() {     int n;     cin>>n;     int arr[n],k=0;     for (int i=0;i<n;i++)     {         cin>>arr[i];         if(arr[i]%2==0)         {             k=k+arr[i];
            }
        }
        cout<<k;
    }

---
+++ Even_Sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{ 
+    int n; 
+    cin>>n; 
+    int arr[n],k=0; 
+    for (int i=0;i<n;i++)
+    { 
+        cin>>arr[i]; 
+        if(arr[i]%2==0)
+        {
+            k=k+arr[i]; 
+            
+        }
+    }
+    cout<<k;
+}
