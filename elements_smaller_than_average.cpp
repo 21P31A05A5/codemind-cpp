815ada0d899c4f628e073c5f317adcff08e2d51b
Pashupati Nath Mishra <97664109+21P31A05A5@users.noreply.github.com>
2022-11-27 05:51:43 -0800

    #include<iostream> using namespace std; int main() {     int n;     cin>>n;     int arr[n],count=0;     int avg,sum=0;     for(int i=0;i<n;i++)     {         cin>>arr[i];         sum=sum+arr[i];     }     avg=sum/n;    // cout<<avg;     for(int i=0;i<n;i++)     {         if(arr[i]<=avg)         {             count=count+1;         }     }     cout<<count; }

---
+++ elements_smaller_than_average.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n],count=0;
+    int avg,sum=0;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        sum=sum+arr[i];
+    }
+    avg=sum/n;
+   // cout<<avg;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<=avg)
+        {
+            count=count+1;
+        }
+    }
+    cout<<count;
+}
